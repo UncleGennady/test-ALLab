ff0b18038607183d6dfd59615a0154dfeccfdc12
Aleksandr Protasenko <96978077+UncleGennady@users.noreply.github.com>
2022-11-20 18:04:38 +0200

    fix pagination's style

--- src/components/pagination/index.tsx
+++ src/components/pagination/index.tsx
@@ -31,19 +31,19 @@ const Pagination = ({jobsPerPage, totalJobs, currentPage, setCurrentPage}:IPagin
     const handleNumberPage = (i:any) => typeof(i) ==="string" ? setCurrentPage(+(i.split('').filter((i:string) => i !=='.').join(''))) : setCurrentPage(i)
 
     return (
-            <div className=" wrapper-pages flex justify-center">
+            <div className="wrapper-pages flex justify-center">
                 <nav className="isolate inline-flex -space-x-px rounded-md shadow-sm font-bold bg-white" aria-label="Pagination">
                     <button
                         onClick={currentPage > 1 ? setCurrentPage(currentPage-1) : null}
                         className="relative inline-flex items-center rounded-l-md bg-white pl-6 py-3 text-sm text-gray-500 hover:bg-gray-50 focus:z-20"
                     >
-                        <div className='inline-block pr-7 border-r-2 border-gray-300'>
+                        <div className='hidden sm:inline-block pr-7 border-r-2 border-gray-300'>
                             <span className="sr-only">Previous</span>
                             <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
                         </div>
                     </button>
 
-                    <div className='px-14 '>
+                    <div className='px-10 md:px-14 '>
                         {getVisiblePages(currentPage, pageNumbers.length).map((i:any, index:number)=>
                             <button
                                 key={index}
@@ -60,7 +60,7 @@ const Pagination = ({jobsPerPage, totalJobs, currentPage, setCurrentPage}:IPagin
                         onClick={currentPage < pageNumbers.at(-1) ? setCurrentPage(currentPage+1) : null}
                         className="relative inline-flex items-center rounded-r-md bg-white pr-6 py-3 text-sm text-gray-500 hover:bg-gray-50 focus:z-20"
                     >
-                        <div className='inline-block pl-7 border-l-2 border-gray-300'>
+                        <div className='hidden sm:inline-block pl-7 border-l-2 border-gray-300'>
                             <span className="sr-only">Next</span>
                             <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
                         </div>
--- src/store/jobApi.ts
+++ src/store/jobApi.ts
@@ -1,5 +1,5 @@
 import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
-
+//process.env.REACT_APP_API_URL//process.env.REACT_APP_TOKEN
 export const jobApi = createApi({
     reducerPath:"jobApi",
     baseQuery:fetchBaseQuery({baseUrl:process.env.REACT_APP_API_URL}),
